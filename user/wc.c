af549e59464e9c5f00eeaf2219dfc4f62a7e8082
Noridom1 <thinhphuc2005@gmail.com>
2026-02-11 16:38:10 +0700

    wc cleaning

--- user/wc.c
+++ user/wc.c
@@ -6,105 +6,109 @@
 
 char buf[512];
 
+bool
+compareArg(const char *a, const char *b)
+{
+  while (*a && *b) {
+    if (*a != *b)
+      return false;
+    a++;
+    b++;
+  }
+  return *a == *b;
+}
+
 void
 printResult(int l, int w, int c, const char *name,
             bool cnt_l, bool cnt_w, bool cnt_c)
 {
-  int printed = 0;
-  if (cnt_l) { printf("%d", l); printed = 1; }
-  if (cnt_w) { printf("%s%d", printed ? " " : "", w); printed = 1; }
-  if (cnt_c) { printf("%s%d", printed ? " " : "", c); }
-  printf("%s%s\n", name[0] ? " " : "", name);
+  if (cnt_l) printf("%d ", l);
+  if (cnt_w) printf("%d ", w);
+  if (cnt_c) printf("%d ", c);
+  if (name[0])
+    printf("%s", name);
+  printf("\n");
 }
 
 void
-wc(int fd, char *name, bool cnt_l, bool cnt_w, bool cnt_c)
+wc(int fd, const char *name, bool cnt_l, bool cnt_w, bool cnt_c)
 {
-  int i, n;
-  int l, w, c, inword;
+  int n;
+  int l = 0, w = 0, c = 0;
+  int inword = 0;
 
-  l = w = c = 0;
-  inword = 0;
-  while((n = read(fd, buf, sizeof(buf))) > 0){
-    for(i=0; i<n; i++){
+  while ((n = read(fd, buf, sizeof(buf))) > 0) {
+    for (int i = 0; i < n; i++) {
       c++;
-      if(buf[i] == '\n')
+
+      if (buf[i] == '\n')
         l++;
-      if(strchr(" \r\t\n\v", buf[i]))
+
+      if (buf[i] == ' ' || buf[i] == '\n' || buf[i] == '\t' ||
+          buf[i] == '\r' || buf[i] == '\v') {
         inword = 0;
-      else if(!inword){
+      } else if (!inword) {
         w++;
         inword = 1;
       }
     }
   }
-  if(n < 0){
-    printf("wc: read error\n");
+
+  if (n < 0) {
+    fprintf(2, "wc: read error\n");
     exit(1);
   }
-  printResult(l, w, c, name, cnt_l, cnt_w, cnt_c);
-}
 
-bool
-compareArg(const char *inp, const char *arg) 
-{
-  while (*inp && *arg) {
-    if (*inp != *arg) return false;
-    inp++;
-    arg++;
-  }
-  return *inp == *arg;
+  printResult(l, w, c, name, cnt_l, cnt_w, cnt_c);
 }
 
 int
 main(int argc, char *argv[])
 {
-  int fd, i;
-  // printf("Number of args: %d\n", argc);
-  // if(argc <= 1){
-  //   // wc(0, "");
-  //   wc(0, "", true, true, true);
-  //   // exit(0);
-    
-  // } 
-  bool l = 0, w = 0, c = 0;
+  int fd;
+  bool cnt_l = false, cnt_w = false, cnt_c = false;
   bool saw_file = false;
-  for(i = 1; i < argc; i++){
-    if (argv[i][0] == '-') {
-      if (compareArg(argv[i], "-l")) l = true;
-      if (compareArg(argv[i], "-w")) w = true;
-      if (compareArg(argv[i], "-c")) c = true;
+
+  // Parse options
+  for (int i = 1; i < argc; i++) {
+    if (argv[i][0] != '-')
+      continue;
+
+    if (compareArg(argv[i], "-l")) cnt_l = true;
+    else if (compareArg(argv[i], "-w")) cnt_w = true;
+    else if (compareArg(argv[i], "-c")) cnt_c = true;
+    else {
+      fprintf(2, "wc: unknown option %s\n", argv[i]);
+      exit(1);
     }
   }
 
-  if (!l && !w && !c) {
-    l = true;
-    w = true;
-    c = true;
-  }
+  // Default: -l -w -c
+  if (!cnt_l && !cnt_w && !cnt_c)
+    cnt_l = cnt_w = cnt_c = true;
 
-  for (int i = 1; i < argc; ++i) {
-    // if (argv[i][0] == '-') continue;
+  // Process files
+  for (int i = 1; i < argc; i++) {
     if (argv[i][0] == '-') {
       if (argv[i][1] == '\0') {
-        wc(0, "", l, w, c);
-        saw_file=true;
+        wc(0, "", cnt_l, cnt_w, cnt_c);
+        saw_file = true;
       }
       continue;
     }
-    
-    saw_file = true;
 
-    if((fd = open(argv[i], O_RDONLY)) < 0){
-      printf("wc: cannot open %s\n", argv[i]);
+    if ((fd = open(argv[i], O_RDONLY)) < 0) {
+      fprintf(2, "wc: cannot open %s\n", argv[i]);
       exit(1);
     }
-    wc(fd, argv[i], l, w, c);
+
+    wc(fd, argv[i], cnt_l, cnt_w, cnt_c);
     close(fd);
+    saw_file = true;
   }
 
-  if(!saw_file) {
-    wc(0, "", l, w, c);
-  }
+  if (!saw_file)
+    wc(0, "", cnt_l, cnt_w, cnt_c);
+
   exit(0);
 }
